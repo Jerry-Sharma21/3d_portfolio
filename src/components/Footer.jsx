2dfb8df9bd96c9b8890ffd36edac0433f9924bd4
Abhijeet <abhijeet.eder.io>
2024-04-15 01:08:04 +0530

    footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,35 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import { Link } from "react-router-dom";
+import { HiDownload } from 'react-icons/hi';
+
+import { socialLinks } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className='footer font-poppins flex flex-row'>
+      <hr className='border-slate-200' />
+      <div className='footer-container'>
+        <div className='flex gap-3 justify-center items-center'>
+          {socialLinks.map((link) => (
+            <Link key={link.name} to={link.link} target='_blank'>
+              <img
+                src={link.iconUrl}
+                alt={link.name}
+                className='w-6 h-6 object-contain'
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+        <a
+          className="px-7 py-3 flex items-center gap-2 rounded-full cursor-pointer borderBlack blueBackground"
+          href="/CV.pdf"
+          download
+        >
+          Download CV
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/constants/index.js
+++ src/constants/index.js
@@ -151,12 +151,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/Jerry-Sharma21',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/abhijeet-sharma-468b00234',
     }
 ];
 
